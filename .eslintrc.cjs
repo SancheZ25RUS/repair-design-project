063a2eb7de540f5784216a0ff2991771016c175d
SancheZ25RUS <sn_elh-92@mail.ru>
2022-10-16 21:36:40 +1000

    add: eslint configuration file

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'max-len': [
+      'off', { 'code': 80 },
+    ],
+    'object-curly-spacing': [
+      'error', 'always',
+    ],
+    'require-jsdoc': 0,
+    'no-unused-vars': 1,
+    'no-undef': 1,
+    'quotes': 1,
+    'camelcase': 0,
+    'indent': 0,
+  },
+};
