22cc614f2babd40cfea3b456d5d2b01941593289
SancheZ25RUS <sn_elh-92@mail.ru>
2022-10-26 01:23:08 +1000

    add: eslint configuration, install last eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,28 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  "env": {
+    "browser": true,
+    "es2021": true,
   },
-  'extends': 'google',
-  'overrides': [
+  "extends": "eslint:recommended",
+  "overrides": [
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
   },
-  'rules': {
-    'max-len': [
-      'off', { 'code': 80 },
+  "rules": {
+    "max-len": [
+      "off", { "code": 80 },
     ],
-    'object-curly-spacing': [
-      'error', 'always',
+    "object-curly-spacing": [
+      "error", "always",
     ],
-    'require-jsdoc': 0,
-    'no-unused-vars': 1,
-    'no-undef': 1,
-    'quotes': 1,
-    'camelcase': 0,
-    'indent': 0,
+    "require-jsdoc": 0,
+    "no-unused-vars": 1,
+    "no-undef": 1,
+    "quotes": 1,
+    "camelcase": 0,
+    "indent": 0,
   },
 };
